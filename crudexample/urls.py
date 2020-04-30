a4c7d8f3f77a9577a05483cab8dcd5bf71382ff3
dev_mun <mundra.devish@gmail.com>
2020-04-30 11:26:51 -0600

    CURD API Example Update

--- crudexample/urls.py
+++ crudexample/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from .api import router
+from person import api_views
 
 
 urlpatterns = [
       path('admin/', admin.site.urls),
       path('api/v1/', include(router.urls)),
+
 ]
--- person/api_views.py
+++ person/api_views.py
@@ -1,10 +1,24 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins
 from . import models
 from . import serializers
+from rest_framework.permissions import IsAdminUser
 
 
-class PeopleViewset(viewsets.ModelViewSet):
+# class PeopleViewset(generics.ListCreateAPIView):
+#     queryset = models.Person.objects.all()
+#     serializer_class = serializers.Personserializer
+#
+# class PersonDetail(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = models.Person.objects.all()
+#     serializer_class = serializers.
+
+class PeopleViewset(mixins.ListModelMixin, mixins.CreateModelMixin, viewsets.GenericViewSet):
     queryset = models.Person.objects.all()
     serializer_class = serializers.Personserializer
+    permission_classes = [IsAdminUser]
 
-#lass PeopleViewSet(mixins.Li)
+    def get(self, request):
+        return self.list(request)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
